efe8868fe137dd3c8a52a02ad044c762196d3e4e
PankajShejul <51691226+PankajShejul@users.noreply.github.com>
2026-09-11 23:24:41 +0530

    Time: 1 ms (99.97%), Space: 42.7 MB (45.14%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+        while (x != 0) {
+            // Overflow check before multiplying by 10
+            if (reversed > Integer.MAX_VALUE / 10
+                || reversed < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        return reversed;
+    }
+}
